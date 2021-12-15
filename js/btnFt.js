786fce1f39407e0d990a65f7d5a6cc08d9cf3169
Nhat <dovannhatdn94@gmail.com>
2021-12-15 20:13:47 +0700

    up

---
+++ js/btnFt.js
@@ -0,0 +1,63 @@
+document.getElementById("btnBai1").onclick = function () {
+    document.querySelector(".form_1").style.display = "block";
+    document.querySelector(".form_2").style.display = "none";
+    document.querySelector(".form_3").style.display = "none";
+    document.querySelector(".form_4").style.display = "none";
+    document.querySelector(".form_5").style.display = "none";
+    document.querySelector(".form_6").style.display = "none";
+    document.querySelector(".form_7").style.display = "none";
+}
+document.getElementById("btnBai2").onclick = function () {
+    document.querySelector(".form_1").style.display = "none";
+    document.querySelector(".form_2").style.display = "block";
+    document.querySelector(".form_3").style.display = "none";
+    document.querySelector(".form_4").style.display = "none";
+    document.querySelector(".form_5").style.display = "none";
+    document.querySelector(".form_6").style.display = "none";
+    document.querySelector(".form_7").style.display = "none";
+}
+document.getElementById("btnBai3").onclick = function () {
+    document.querySelector(".form_1").style.display = "none";
+    document.querySelector(".form_2").style.display = "none";
+    document.querySelector(".form_3").style.display = "block";
+    document.querySelector(".form_4").style.display = "none";
+    document.querySelector(".form_5").style.display = "none";
+    document.querySelector(".form_6").style.display = "none";
+    document.querySelector(".form_7").style.display = "none";
+}
+document.getElementById("btnBai4").onclick = function () {
+    document.querySelector(".form_1").style.display = "none";
+    document.querySelector(".form_2").style.display = "none";
+    document.querySelector(".form_3").style.display = "none";
+    document.querySelector(".form_4").style.display = "block";
+    document.querySelector(".form_5").style.display = "none";
+    document.querySelector(".form_6").style.display = "none";
+    document.querySelector(".form_7").style.display = "none";
+}
+document.getElementById("btnBai5").onclick = function () {
+    document.querySelector(".form_1").style.display = "none";
+    document.querySelector(".form_2").style.display = "none";
+    document.querySelector(".form_3").style.display = "none";
+    document.querySelector(".form_4").style.display = "none";
+    document.querySelector(".form_5").style.display = "block";
+    document.querySelector(".form_6").style.display = "none";
+    document.querySelector(".form_7").style.display = "none";
+}
+document.getElementById("btnBai6").onclick = function () {
+    document.querySelector(".form_1").style.display = "none";
+    document.querySelector(".form_2").style.display = "none";
+    document.querySelector(".form_3").style.display = "none";
+    document.querySelector(".form_4").style.display = "none";
+    document.querySelector(".form_5").style.display = "none";
+    document.querySelector(".form_6").style.display = "block";
+    document.querySelector(".form_7").style.display = "none";
+}
+document.getElementById("btnBai7").onclick = function () {
+    document.querySelector(".form_1").style.display = "none";
+    document.querySelector(".form_2").style.display = "none";
+    document.querySelector(".form_3").style.display = "none";
+    document.querySelector(".form_4").style.display = "none";
+    document.querySelector(".form_5").style.display = "none";
+    document.querySelector(".form_6").style.display = "none";
+    document.querySelector(".form_7").style.display = "block";
+}
